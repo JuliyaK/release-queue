c11bbf6077a7651bbb32b6be1829c3776f339b2a
JuliyaK <y.kashlakova@yandex.ru>
2019-01-06 19:31:54 +0700

    Commit_one

--- index.js
+++ index.js
@@ -1,178 +1,153 @@
-import React from 'react';
+import React, {Suspense, Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import {unstable_createResource} from 'react-cache';
 import {Responsive, WidthProvider} from 'react-grid-layout';
 import './index.css';
+import { ReadStream } from 'fs';
 
-function Header() {
+
+function AppHeader() {
     return <h3>Таблица очереди релизов</h3>
 }
 
-var queue =[
-    {
-        "key": "RELEASE-0001",
-        "name": "Application1",
-        "status": "waiting",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0012",
-        "name": "Application1",
-        "status": "waiting",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0002",
-        "name": "Application1",
-        "status": "testing",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0011",
-        "name": "Application1",
-        "status": "testing",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0003",
-        "name": "Application2",
-        "status": "waiting",
-        "type": "frontend"
-    },
-    {
-        "key": "RELEASE-0010",
-        "name": "Application1",
-        "status": "building",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0004",
-        "name": "Application1",
-        "status": "testing",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0005",
-        "name": "Application2",
-        "status": "deploying",
-        "type": "frontend"
-    },
-    {
-        "key": "RELEASE-0013",
-        "name": "Application2",
-        "status": "deploying",
-        "type": "frontend"
-    },
-    {
-        "key": "RELEASE-0015",
-        "name": "Application2",
-        "status": "deploying",
-        "type": "frontend"
-    },
-    {
-        "key": "RELEASE-0006",
-        "name": "Application1",
-        "status": "stabilizing",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0014",
-        "name": "Application1",
-        "status": "stabilizing",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0016",
-        "name": "Application1",
-        "status": "stabilizing",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0007",
-        "name": "Application1",
-        "status": "building",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0008",
-        "name": "Application1",
-        "status": "building",
-        "type": "backend"
-    },
-    {
-        "key": "RELEASE-0009",
-        "name": "Application1",
-        "status": "testing",
-        "type": "backend"
-    },
-] ;
 
-function ReleaseQueue(item) {
-    switch(item.status) {
-       
-        case "testing":
-            var fullname = [item.key,item.name,item.type].join(" ");
-            console.log(fullname)
-            return (    
-                <div class="grid-item testing" key={item.key}>{fullname}</div>
-            );
-            break;
+
+/*
+var queue =[{
     
-        case "waiting":
-            var fullname = [item.key,item.name,item.type].join(" ");
-            console.log(fullname)
-            return (
-                <div class="grid-item waiting"  key={item.key}>{fullname}</div>    
-            );
-            break;
-
-        case "building":
-            var fullname = [item.key,item.name,item.type].join(" ");
-            console.log(fullname)
-            return (
-                <div class="grid-item building" key={item.key}>{fullname}</div>
-            );
-            break;
-
-        case "deploying":
-            var fullname = [item.key,item.name,item.type].join(" ");
-            console.log(fullname)
-            return (
-                <div class="grid-item deploying" key={item.key}>{fullname}</div>
-            );
-            break;
-
-        case "stabilizing":
-            var fullname = [item.key,item.name,item.type].join(" ");
-            console.log(fullname)
-            return ( 
-                <div class="grid-item stabilizing" key={item.key}>{fullname}</div>              
-            );
-            break;
+        key:  "RELEASE-0001",
+        name:   "Application1",
+        status:  "waiting",
+        type:   "backend"
+    },
+    {
+        key: "RELEASE-0002",
+        name: "Application2",
+        status: "waiting",
+        type: "frontend"
+    },
+    {
+        key: "RELEASE-0003",
+        name: "Application1",
+        status: "building",
+        type: "backend"
+    },
+    {
+        key: "RELEASE-0004",
+        name: "Application1",
+        status: "testing",
+        type: "backend"
+    },
+    {
+        key: "RELEASE-0005",
+        name: "Application2",
+        status: "building",
+        type: "frontend"
+    },
+    {
+        key: "RELEASE-0006",
+        name: "Application2",
+        status: "deploying",
+        type: "frontend"
+    },
+    {
+        key: "RELEASE-0008",
+        name: "Application1",
+        status: "stabilizing",
+        type: "frontend"
+    },
+    {
+        key: "RELEASE-0009",
+        name: "Application2",
+        status: "stabilizing",
+        type: "backend"
+    },
+    {
+        key: "RELEASE-0010",
+        name: "Application2",
+        status: "stabilizing",
+        type: "frontend"
     }
-}
+]*/
 
-function DataOutput()
-{
-    const items = queue.map(ReleaseQueue);
-    console.log(items);
-    return (
-    <div class ="grid-container">
-        <div class="grid-items">Ожидание</div>
-        <div class="grid-items">Сборка</div>
-        <div class="grid-items">Тестирование</div>
-        <div class="grid-items">Развертывание</div>
-        <div class="grid-items">Стабилизация</div>
-        {items}
-    </div> 
-    );
-}
+const Fetcher = unstable_createResource (() =>
+fetch("http://localhost:3001/tasks").then(response => response.json())
+);
+const getData = () => Fetcher.read();
+const Release_queue = () => {
+    const queue = getData();
+  
+        var waitingGroup = [];
+        var buildingGroup = [];
+        var testingGroup = [];
+        var deployingGroup = [];
+        var stabilizingGroup = [];
 
+            for(var i = 0; i < queue.length; i++)
+            {
+                var fullname = [queue[i].key, queue[i].name, queue[i].type].join(" ");
+                if(queue[i].status === "waiting")
+                {
+                    waitingGroup.push(<div class="grid-container waiting">{fullname}</div>); 
+                }
+                if(queue[i].status === "building")
+                {
+                    buildingGroup.push( <div class="grid-container building">{fullname}</div>);    
+                }
+                if(queue[i].status === "testing")
+                {
+                    testingGroup.push(<div class="grid-container testing">{fullname}</div>);   
+                }
+                if(queue[i].status === "deploying")
+                {
+                    deployingGroup.push(<div class="grid-container deploying">{fullname}</div>);  
+                }
+                if(queue[i].status === "stabilizing")
+                {
+                    stabilizingGroup.push(<div class="grid-container stabilizing">{fullname}</div>);   
+                }
+            }
+        }
+                    
+
+
+function Container() {
+    return(
+        <div class="container">
+                <div class="grid-container">
+                    <div class="grid-container faza1">ОЖИДАНИЕ</div>
+                    <div class="grid-container faza2">СБОРКА</div>
+                    <div class="grid-container faza3">ТЕСТИРОВАНИЕ</div>
+                    <div class="grid-container faza4">РАЗВЕРТЫВАНИЕ</div>
+                    <div class="grid-container faza5">СТАБИЛИЗАЦИЯ</div>
+                </div>
+                <div class="task">
+                    <div class="task one"> 
+                      {waitingGroup}
+                    </div>
+                    <div class="task two">
+                        {buildingGroup}
+                    </div>
+                    <div class="task three">
+                        {testingGroup}
+                    </div>
+                    <div class="task four">
+                        {deployingGroup}
+                    </div>
+                    <div class="task five">
+                        {stabilizingGroup}
+                    </div>
+                </div>
+        </div>               
+    )
+}
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <DataOutput/>
+        <div>   
+         <AppHeader/>
+         <Container/>
+         
         </div>
     );
 }
@@ -181,4 +156,3 @@ ReactDOM.render(
     <App/>,
     document.getElementById('root')
 );
-
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const tasks = require('./tasks.json');
+var cors = require('cors');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+app.use(cors());
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+app.listen(3001, () => {
+    console.log('Application started on port 3000');
+});
