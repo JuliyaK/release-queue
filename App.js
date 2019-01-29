335795f0ac910d086c0ae4c2fdc983152232d37a
JuliyaK <y.kashlakova@yandex.ru>
2019-01-29 20:47:03 +0800

    v5

--- App.js
+++ App.js
@@ -3,23 +3,76 @@ import './App.css';
 import AppHeader from './components/AppHeader'
 import Container from './components/contsiners'
 
+
 class App extends React.Component {
 
-gettingDate = async () => {
-  
-  const api_url = await fetch("http://localhost:3001/tasks");
-  const data = await api_url.json();
-  console.log(data);
-  
+state = {
+  waiting: null,
+  testing: null
+ 
 }
 
+
+gettingDate = async (e) => {
+e.preventDefault();
+  const api_url = await fetch(`http://localhost:3001/tasks`);
+  const data = await api_url.json();
+  console.log(data);
+
+
+        var waitingGroup = [];
+        var buildingGroup = [];
+        var testingGroup = [];
+        var deployingGroup = [];
+        var stabilizingGroup = [];
+
+            for(var i = 0; i < data.length; i++)
+            {
+                var fullname = [data[i].key, data[i].name, data[i].type].join(" ");
+                if(data[i].status === "waiting")
+                {
+                    waitingGroup.push({fullname}); 
+                }
+                if(data[i].status === "building")
+                {
+                    buildingGroup.push({fullname});    
+                }
+                if(data[i].status === "testing")
+                {
+                    testingGroup.push({fullname});   
+                }
+                if(data[i].status === "deploying")
+                {
+                    deployingGroup.push({fullname});  
+                }
+                if(data[i].status === "stabilizing")
+                {
+                    stabilizingGroup.push({fullname});   
+                }
+            }
+      console.log(waitingGroup, buildingGroup, testingGroup, deployingGroup, stabilizingGroup)
+
+this.setState({
+ waiting:  waitingGroup,
+ testing: testingGroup
+ 
+});
+console.log(this.state)
+
+}
   render() {
     return (
       <div>
         <AppHeader />
-        <Container gettingMetod={this.gettingDate()} />
+        <Container getDate={this.gettingDate} 
+       
+       
+        />
       </div>
+      
     );
+    
   }
+
 }
 export default App;
--- contsiners.js
+++ contsiners.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
+
 class Container extends React.Component {
     render() {
         return(
-            <div onSubmit={this.props.gettingMetod}>
+            <form onSubmit={this.props.getDate}>
             <button>Получить данные</button>
+            
                 <div className="grid-container">
-                    <div className="grid-container faza1">ОЖИДАНИЕ</div>
+                    <div className="grid-container faza1">ОЖИДАНИЕ 
+                    
+                    
+                    </div>
                     <div className="grid-container faza2">СБОРКА</div>
                     <div className="grid-container faza3">ТЕСТИРОВАНИЕ</div>
                     <div className="grid-container faza4">РАЗВЕРТЫВАНИЕ</div>
                     <div className="grid-container faza5">СТАБИЛИЗАЦИЯ</div>
+                    
                 </div>
                 <div className="task">
                     <div className="task one"></div>
@@ -18,9 +24,11 @@ class Container extends React.Component {
                     <div className="task three"></div> 
                     <div className="task four"></div>
                     <div className="task five"></div>
+
+                   
                 </div>
                 
-            </div>               
+            </form>               
         );
     }
 }
